8d50dda2e4d1aa8b8bc233735529700d3fb20825
Sawyer <sawyer9smith@gmail.com>
2021-02-04 11:32:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideVsNewKeywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass bc = new BaseClass();
+            DerivedClass dc = new DerivedClass();
+            BaseClass bcdc = new DerivedClass();
+
+            bc.Method1();
+            dc.Method1();
+            dc.Method2();
+            bcdc.Method1();
+        }
+        // Output:  
+        // Base - Method1  
+        // Base - Method1  
+        // Derived - Method2  
+        // Base - Method1       
+    }
+
+    class BaseClass
+                {
+                    public void Method1()
+                    {
+                        Console.WriteLine("Base - Method1");
+                    }
+                }
+
+                class DerivedClass : BaseClass
+                {
+                    public void Method2()
+                    {
+                        Console.WriteLine("Derived - Method2");
+                    }
+                }
+    }
+    
+
